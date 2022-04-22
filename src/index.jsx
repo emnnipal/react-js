7b2513015efb09904c51bb4eeb6eaad4d0cfc53c
Emmanuel Nipal <emnnipal@gmail.com>
2022-04-22 10:26:40 +0800

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import './index.scss';
+import App from './App';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
